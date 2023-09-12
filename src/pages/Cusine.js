de251fae6f79db6cb93a2ea5801ff3fafec9fa53
Jecy Jehu <jecyjehu@outlook.com>
2023-09-12 17:23:33 +0100

    deploy

--- src/pages/Cusine.js
+++ src/pages/Cusine.js
@@ -1,6 +1,6 @@
 // import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 
 const Cusine = () => {
@@ -32,11 +32,13 @@ const Cusine = () => {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-3 justify-center">
         {cuisine.map((results) => (
           <div key={results.id}>
-            <div className="relative">
-              <div className="w-[100%] h-[100%] bottom-0 rounded-3xl absolute z-[3] bg-gradient-to-b from-dark/20 to-dark/40" />          
-              <img src={results.image} alt="/" width={400} height={250} priority="true" className="w-auto h-auto text-center border rounded-3xl" />
-            </div>
-            <h2 className="text-sm font-semibold">{results.title}</h2>
+            <Link to={'/details/' + results.id}>
+              <div className="relative">
+                <div className="w-[100%] h-[100%] bottom-0 rounded-3xl absolute z-[3] bg-gradient-to-b from-dark/20 to-dark/40" />          
+                <img src={results.image} alt="/" width={400} height={250} priority="true" className="w-auto h-auto text-center border rounded-3xl" />
+              </div>
+              <h2 className="text-sm font-semibold">{results.title}</h2>
+            </Link>
           </div>
         ))}
       </div>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 const Details = () => {
     const params = useParams();
     const [details, setDetails] = useState([]);
+    const [active, setActive] = useState('instructions');
 
     useEffect(() => {
         async function fetchDetails() {
@@ -24,16 +25,43 @@ const Details = () => {
 
   return (
     <section className="w-full h-auto my-9 max-w-[380px] sm:max-w-[550px] md:max-w-[1100px] m-auto">
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-5">
-            <div className="relative">
-                <div className="w-[100%] h-[100%] bottom-0 rounded-3xl absolute z-[3] bg-gradient-to-b from-dark/20 to-dark/40" />
-                <img src={details.image} alt='/' />
+        <div className="grid md:grid-cols-12 space-y-4 md:space-y-0 md:space-x-5">
+            <div className="md:col-span-5 space-y-3">
+                <h1 className="text-lg font-semibold">{details.title}</h1>
+                <div className="relative">
+                    <div className="w-[100%] h-[100%] bottom-0 absolute z-[3] bg-gradient-to-b from-dark/20 to-dark/40" />
+                    <img src={details.image} alt='/' width={1000} height={1000} />
+                </div>
             </div>
-            <div className="space-x-3">
-                <button className="border bg-dark rounded-lg h-9 w-28 text-milk hover:bg-white hover:text-dark hover:font-semibold">Information</button>
-                <button className="border-2 bg-milk text-dark font-semibold rounded-lg h-9 w-28 hover:text-milk hover:bg-dark">Ingredients</button>
-            </div>
-            <h1>{details.title}</h1>
+            <div className="md:col-span-7">
+                <div className="space-x-3">
+                    <button 
+                        onClick={() => setActive('instructions')}
+                        className="border bg-dark rounded-lg h-9 w-28 text-milk hover:bg-white hover:text-dark hover:font-semibold"
+                    >
+                        Information
+                    </button>
+                    <button 
+                        onClick={() => setActive('ingredients')}
+                        className="border-2 bg-milk text-dark font-semibold rounded-lg h-9 w-28 hover:text-milk hover:bg-dark"
+                    >
+                        Ingredients
+                    </button>
+                </div>
+                {active === 'instructions' && (
+                    <div>
+                    <h2 dangerouslySetInnerHTML={{__html: details.summary}} className="text-lg"></h2>
+                    <h2 dangerouslySetInnerHTML={{__html: details.instructions}} className="text-lg"></h2>
+                    </div>)
+                }
+                {active === 'ingredients' && (
+                    <ul>
+                        {details.extendedIngredients.map((ingredient) => (
+                        <li key={ingredient.id}>{ingredient.original}</li>
+                        ))}
+                    </ul>)       
+                }
+            </div>          
         </div>
         
     </section>
